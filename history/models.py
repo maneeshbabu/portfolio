26228180e606e9f2bf8479e0ce59a63a61da1a66
Maneesh Babu M <maneesh@amagi.com>
2021-05-29 06:37:50 +0000

    Add projects model and migration

--- history/models.py
+++ history/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='history/images/')
+    url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+    
