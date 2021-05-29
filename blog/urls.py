4711b0de94f7b0a867fcf0b0077eddfbbd6dc3ac
Maneesh Babu M <maneesh@amagi.com>
2021-05-29 07:41:21 +0000

    Add details

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+
+app_name= "blog"
+
 urlpatterns = [
-    path('', views.index, name='bloglist')
+    path('', views.index, name='list'),
+    path('<int:blog_id>', views.show, name='detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Blog
 
@@ -7,3 +7,9 @@ from .models import Blog
 def index(request):
     blogs = Blog.objects.order_by('-created_at')[:5]
     return render(request, 'blogs/index.html', {'blogs': blogs})
+
+
+def show(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blogs/show.html', {'blog': blog})
+    
