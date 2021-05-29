3f6a61bc6736b69db11cf27710f73eb4562bb45a
Maneesh Babu M <maneesh@amagi.com>
2021-05-29 07:20:50 +0000

    Add blogs page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Blog
+
 # Create your views here.
+
+def index(request):
+    blogs = Blog.objects.order_by('-created_at')[:5]
+    return render(request, 'blogs/index.html', {'blogs': blogs})
